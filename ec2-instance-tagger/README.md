c2693eef2f05f191da04ae153b081ae27d66181f
Joshua walker <josh.d.walker@me.com>
2020-10-02 11:47:11 +0100

    add ec2-instance-tagger files

---
+++ ec2-instance-tagger/README.md
@@ -0,0 +1,53 @@
+# Bitbucket EC2 Instance Tagger
+
+Tag running AWS EC2 Instance ready for deployment in Bitbucket pipeline.
+
+## YAML Definition
+
+Add the following snippet to the script section of your `bitbucket-pipelines.yml` file:
+
+```yaml
+- pipe: sykescottages/bitbucket-secrets-manager
+    FILE: '<string>'
+    AWS_ACCESS_KEY_ID: '<string>'
+    AWS_ACCESS_KEY_ID: '<string>'
+    AWS_SECRET_ACCESS_KEY: '<string>'
+    AWS_SECRET_NAME: '<string>'
+    AWS_REGION: '<string>'
+    AWS_PROFILE: '<string>'
+    CONFIG: '<string>'  
+```
+
+## Variables
+
+| Variable              | Usage                                                       |
+| --------------------- | ----------------------------------------------------------- |
+| FILE             | File Name you Wish you save to. (Default .env)|
+| AWS_ACCESS_KEY_ID (*)              | AWS key id. |
+| AWS_SECRET_ACCESS_KEY (*) | AWS secret key. |
+| AWS_SECRET_NAME (*) | The name of the secret. |
+| AWS_REGION (*) | AWS region. |
+| AWS_PROFILE (*) | The name of the AWS profile. eg default, production, non-prod, staging, dev |
+| CONFIG               | Path to AWS config file eg (s3 restricted access) |
+_(*) = required variable. This variable needs to be specified always when using the pipe._
+
+#### Workspaces Variables
+- $AWS_ACCESS_KEY
+- $AWS_SECRET_KEY
+
+
+## Examples
+
+Example pipe yaml
+
+```yaml
+script:
+  - pipe: sykescottages/bitbucket-ec2-instance-tagger
+    variables:
+      AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY
+      AWS_SECRET_ACCESS_KEY: $AWS_SECRET_KEY
+      AWS_SECRET_NAME: sm-s-ew1-project
+      AWS_REGION: eu-west-1
+      AWS_PROFILE: staging
+```
+
--- ec2-instance-tagger/pipe.sh
+++ ec2-instance-tagger/pipe.sh
@@ -3,3 +3,85 @@
 set -e
 set -o pipefail
 
+# required parameters
+APPLICATION_TAG=${APPLICATION_TAG}
+DEPLOY_TAG=${DEPLOY_TAG}
+ACCESS=${AWS_ACCESS_KEY_ID}
+KEY=${AWS_SECRET_ACCESS_KEY}
+
+REGION=${AWS_REGION}
+PROFILE=${AWS_PROFILE}
+CONFIG=${CONFIG:='https://s3-auth-ew1-bitbucket-secrets-manager-bucket-config.s3-eu-west-1.amazonaws.com/config'}
+DEBUG=${DEBUG:=false}
+
+
+create_config(){
+  mkdir aws
+  curl "${CONFIG}" > aws/config
+}
+
+check_variables(){
+
+  if [ ! -e aws/config ]; then
+    echo "Cant not access the AWS Config file."
+    exit 1
+  fi
+
+  if ! [ -n "${ACCESS}" ]; then
+    echo "Set the AWS access key."
+    exit 1
+  fi
+
+  if ! [ -n "${KEY}" ]; then
+    echo "Set the AWS Secret key."
+    exit 1
+  fi
+
+  if ! [ -n "${REGION}" ]; then
+    echo "Provid the AWS Region"
+    exit 1
+  fi
+
+  if ! [ -n "${PROFILE}" ]; then
+    echo "Provid the AWS IAM profile"
+    exit 1
+  fi
+
+}
+
+start(){
+  echo "Tagging"
+}
+
+create_credentials(){
+
+  echo -e "[auth] \n
+      aws_access_key_id = ${ACCESS} \n
+      aws_secret_access_key = ${KEY} \n
+      " > aws/credentials
+}
+
+tag_instance(){
+    export AWS_CONFIG_FILE=aws/config
+    export AWS_SHARED_CREDENTIALS_FILE=aws/credentials
+
+  i=$(aws ec2 describe-instances \
+  --filters "Name=tag:Codedeploy,Values=Tesla" --profile ${PROFILE} --region ${REGION} --query "Reservations[*].Instances[*].[InstanceId]" --output text)
+
+  b=$(aws ec2 describe-instances \
+  --filters "Name=instance-state-code,Values=80" --instance-ids $i  --profile production --query "Reservations[*].Instances[*].[InstanceId]" --output text)
+
+  # aws secretsmanager get-secret-value --secret-id ${SECRET} --query SecretString --output text --region ${REGION}  --profile ${PROFILE}
+  # | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > ${FILE} || { echo 'Failed' ; exit 1; }
+}
+
+completed(){
+  echo "Tagged"
+}
+
+create_config
+check_variables
+start
+create_credentials
+tag_instance
+completed
