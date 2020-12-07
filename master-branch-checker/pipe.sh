f5fbea9b0e1bae4d8adc10869b6a79429d783fc5
Bruno Mota <bruno.mota@sykescottages.co.uk>
2020-12-07 16:03:26 +0000

    Add master branch checker pipeline

---
+++ master-branch-checker/pipe.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+DIFF_FROM_MASTER=$(git rev-list --left-right --count HEAD...origin/master|awk '{print $2}')
+
+echo "----------------------------------------------------------------------------------------------------"
+echo "| SYKES BRANCH CHECKER                                                                             |"
+echo "----------------------------------------------------------------------------------------------------"
+
+if [ "$DIFF_FROM_MASTER" -gt "0" ]; then
+    echo "| Your build has failed because master has $DIFF_FROM_MASTER more changes than your current branch |"
+    echo "|                                                                                                  |"
+    echo "| You need to run 'git pull origin master' on your branch!                                         |"
+    echo "----------------------------------------------------------------------------------------------------"
+    exit 1
+else
+    echo "| Your build is up to date with master well done!                                                   |"
+    echo "----------------------------------------------------------------------------------------------------"
+    exit 0
+fi
