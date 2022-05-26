ce8055ba0dc12ec752cf36eea6fa1377811c49db
Robert Harvey <robert.harvey2580@gmail.com>
2022-05-26 16:19:05 +0100

    Update merge checker to allow for main branch

--- master-branch-checker/pipe.sh
+++ master-branch-checker/pipe.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 set -e
 
-DIFF_FROM_MASTER=$(git rev-list --left-right --count HEAD...origin/master|awk '{print $2}')
+BRANCH_NAME=$(git branch | cut -c 3- | grep -E '^master$|^main$')
+
+DIFF_FROM_MASTER=$(git rev-list --left-right --count HEAD...origin/$BRANCH_NAME|awk '{print $2}')
 
 echo "----------------------------------------------------------------------------------------------------"
 echo "| SYKES BRANCH CHECKER                                                                             |"
 echo "----------------------------------------------------------------------------------------------------"
 
 if [ "$DIFF_FROM_MASTER" -gt "0" ]; then
-    echo "| Your build has failed because master has $DIFF_FROM_MASTER more changes than your current branch |"
+    echo "| Your build has failed because $BRANCH_NAME has $DIFF_FROM_MASTER more changes than your current branch |"
     echo "|                                                                                                  |"
-    echo "| You need to run 'git pull origin master' on your branch!                                         |"
+    echo "| You need to run 'git pull origin $BRANCH_NAME' on your branch!                                         |"
     echo "----------------------------------------------------------------------------------------------------"
     exit 1
 else
-    echo "| Your build is up to date with master well done!                                                   |"
+    echo "| Your build is up to date with $BRANCH_NAME well done!                                                   |"
     echo "----------------------------------------------------------------------------------------------------"
     exit 0
 fi
