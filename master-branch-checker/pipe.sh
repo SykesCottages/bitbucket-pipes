6cbaf3baa8d5b3927417405095b16dc5292ee86c
RobHarveyDev <87716995+RobHarveyDev@users.noreply.github.com>
2025-02-06 14:14:35 +0000

    Update master merge check to allow main branch

--- master-branch-checker/pipe.sh
+++ master-branch-checker/pipe.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 set -e
 
-git config --global --add safe.directory /opt/atlassian/pipelines/agent/build
+BRANCH_NAME=$(git remote show origin | grep "HEAD" | cut -d: -f2 | tr -d ' ')
 
-DIFF_FROM_MASTER=$(git rev-list --left-right --count HEAD...origin/master|awk '{print $2}')
+DIFF_FROM_MASTER=$(git rev-list --left-right --count HEAD...origin/$BRANCH_NAME|awk '{print $2}')
 
 echo "----------------------------------------------------------------------------------------------------"
 echo "| SYKES BRANCH CHECKER                                                                             |"
 echo "----------------------------------------------------------------------------------------------------"
 
 if [ "$DIFF_FROM_MASTER" -gt "0" ]; then
-    echo "| Your build has failed because master has $DIFF_FROM_MASTER more changes than your current branch |"
+    echo "| Your build has failed because $BRANCH_NAME has $DIFF_FROM_MASTER more changes than your current branch |"
     echo "|                                                                                                  |"
-    echo "| You need to run 'git pull origin master' on your branch!                                         |"
+    echo "| You need to run 'git pull origin $BRANCH_NAME' on your branch!                                         |"
     echo "----------------------------------------------------------------------------------------------------"
     exit 1
 else
-    echo "| Your build is up to date with master well done!                                                   |"
+    echo "| Your build is up to date with $BRANCH_NAME well done!                                                   |"
     echo "----------------------------------------------------------------------------------------------------"
     exit 0
 fi
